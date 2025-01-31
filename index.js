9b535ce0429eff28caf28a26caf724f7d88703e4
mahanandhan <mahanandhanmanish@gmail.com>
2025-01-31 11:19:34 +0530

    added

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const app = express();
 const port = 3010;
+
+// MongoDB connection
+const mongoDB = process.env.DB_URI; // replace with your MongoDB URI
+
+// Async function to handle MongoDB connection
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoDB);
+    console.log(mongoDB)
+    console.log('Successfully connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+};
+
+// Connect to MongoDB when the app starts
+connectToDatabase();
 
 app.use(express.static('static'));
 
@@ -11,5 +30,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http:localhost:${port}`);
 });
+
+// c6aWA8XFcob0CHSy
